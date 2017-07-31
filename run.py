3138f2c5bb9a664d408cf6ae87fc25bd56ff8c3a
Taylor Liang <bu213200@gmail.com>
2017-07-31 11:49:52 +0800

    Add new strategy

--- run.py
+++ run.py
@@ -1,55 +1,108 @@
-import re
+from sys import platform
 import sys
 import datetime
 import time
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
-def GetTimeStr(time):
-    return str(now.year % 100) + (('0' + str(now.month)) if now.month < 10 else str(now.month)) + str(now.day)
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-def GetTimeStr2(time):
-    return str(now.year) + '-' + (('0' + str(now.month)) if now.month < 10 else str(now.month)) + '-' + str(now.day)
+def GetUrlTime(now):
+    return str(now.year % 100) + \
+           (('0' + str(now.month)) if now.month < 10 else str(now.month)) + \
+           (('0' + str(now.day)) if now.day < 10 else str(now.day))
 
 
-if __name__ == "__main__":
-    now = datetime.datetime.now()
-    nowString = GetTimeStr(now)
+def GetScriptTime(now):
+    return str(now.year) + '-' + \
+           (('0' + str(now.month)) if now.month < 10 else str(now.month)) + '-' + \
+           (('0' + str(now.day)) if now.day < 10 else str(now.day))
 
-    with open("LowPriceOutput.txt", "a") as myfile:
-        myfile.write(nowString + " (CheckDate): \n")
 
-    for i in range(0, 180):
-        nowString = GetTimeStr(now)
-        print('Start retrieving ' + nowString)
-        page = requests.get('http://www.flycua.com/flight2014/nay-hny-' + nowString + '_CNY.html')
-        url = 'http://www.flycua.com/flight2014/nay-hny-' + nowString + '_CNY.html'
+def page_has_loaded(deiver):
+    page_state = deiver.execute_script('return document.readyState')
+    print('page_state: ' + page_state)
+    return page_state == 'complete'
 
-        driver = webdriver.PhantomJS(
-            "E:\Dropbox\Dropbox\Soft\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\\bin\phantomjs.exe", 0)
-        driver.get(url)
 
-        html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-        soup = BeautifulSoup(html, 'html.parser')
-        info = soup.find_all('li', class_='active', title=GetTimeStr2(now))
-        print(info)
-        if str(info).__len__() >= 2:
-            soup2 = BeautifulSoup(str(info)[1:-1], 'html.parser')
-            info2 = soup2.find_all('b')
-            print(info2)
-            print('End retrieving ' + nowString)
-            if str(info2).__len__() >= 10:
-                price = int(str(info2)[5:-5].replace(',', ''))
+def WebsiteExecute(tag, count, now, gap, critical, fileWrite):
+    isCritical = False
 
-                if price <= 500:
-                    with open("LowPriceOutput.txt", "a") as myfile:
-                        myfile.write("nay-hny-" + nowString + ", Price: " + price + "\n")
+    for i in range(0, count):
+        try:
+            nowString = GetUrlTime(now)
+            url = 'http://www.flycua.com/flight2014/' + tag + nowString + '_CNY.html'
+            print('Start retrieving ' + tag + nowString)
 
-        driver.quit()
+            if platform == 'win32':
+                driver = webdriver.PhantomJS(
+                    'E:\Dropbox\Dropbox\Soft\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\\bin\phantomjs.exe')
+                # driver = webdriver.Chrome('E:\Dropbox\Dropbox\Soft\chromedriver_win32\chromedriver.exe')
+            elif platform == 'linux' or platform == 'linux2':
+                pass
+                # to implement
+            else:
+                print('Abort retrieving cause platform not exist: ' + platform)
+                with open('LowPriceOutput.txt', 'a') as myfile:
+                    myfile.write('Abort retrieving cause platform not exist: ' + platform + '\n')
+                break
 
-        now += datetime.timedelta(days=1)
-        time.sleep(60)
+            driver.get(url)
+            WebDriverWait(driver, 5).until(page_has_loaded)
+
+            html = driver.execute_script('return document.getElementsByTagName("html")[0].innerHTML')
+            soup = BeautifulSoup(html, 'html.parser')
+            info = soup.find_all('li', class_='active', title=GetScriptTime(now))
+            print(info)
+            if str(info).__len__() >= 2:
+                soup2 = BeautifulSoup(str(info)[1:-1], 'html.parser')
+                info2 = soup2.find_all('b')
+                print(info2)
+                print('End retrieving ' + tag + nowString)
+                if str(info2).__len__() >= 10:
+                    price = int(str(info2)[5:-5].replace(',', ''))
+
+                    if price <= critical:
+                        print('Record low price ' + tag + nowString)
+                        fileWrite += tag + nowString + ', Price: ' + str(price) + '\n'
+                        isCritical = True
+
+            driver.quit()
+
+            now += datetime.timedelta(days=1)
+            time.sleep(gap)
+        except:
+            print('Error retrieving ' + tag + nowString + '\n')
+            with open('LowPriceOutput.txt', 'a') as myfile:
+                myfile.write('Error retrieving ' + tag + nowString)
+            continue
+
+    return isCritical
+
+
+if __name__ == '__main__':
+    realNow = datetime.datetime.now()
+    while realNow.weekday() != 2:
+        realNow += datetime.timedelta(days=1)
+
+    for i in range(0, 26):
+        fileWrite = ''
+
+        now = realNow
+        while now.weekday() != 2:
+            now += datetime.timedelta(days=1)
+        nowString = GetUrlTime(now)
+
+        with open('LowPriceOutput.txt', 'a') as myfile:
+            myfile.write(nowString + ' (CheckDate): \n')
+
+        if WebsiteExecute('nay-hny-', 3, now, 60, 500, fileWrite):
+            while now.weekday() != 0:
+                now += datetime.timedelta(days=1)
+            if WebsiteExecute('hny-nay-', 3, now, 60, 500, fileWrite):
+                with open('LowPriceOutput.txt', 'a') as myfile:
+                    myfile.write(fileWrite)
+
+        realNow += datetime.timedelta(days=7)
 
     sys.exit(0)
